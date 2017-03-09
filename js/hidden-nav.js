abfe65cff2ac96ffc2a0e0477fc312999eaa962e
YEAL2 <alice.ye@oocl.com>
2017-03-09 17:37:55 +0800

    init

---
+++ js/hidden-nav.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+	var menuBtn = document.getElementById("menu-btn");
+	var navMenu = document.getElementById("nav-menu");
+	var wrapper = document.getElementById("wrapper");
+	var mask = document.getElementById("mask");
+	var li = navMenu.getElementsByTagName('li');
+	var body = document.getElementsByTagName('body');
+	var flag = 0;
+	menuBtn.addEventListener('click', function(){
+		if(flag == 0){
+			wrapper.className += ' menu-open';
+			navMenu.className += ' nav-menu-open';
+			menuBtn.className += ' active';
+			mask.className += ' active';
+			body.className = 'menu-open';
+			for(var i=0; i<li.length; i++){
+				li[i].className = 'menu-item-open item'+(i+1);
+			}
+			flag = 1;
+		}else {
+			wrapper.className = 'wrapper';
+			navMenu.className = 'nav-menu';
+			menuBtn.className = 'menu-btn';
+			mask.className = 'menu-mask';
+			body.className = '';
+			for(var i=0; i<li.length; i++){
+				li[i].className = 'menu-item';
+			}
+			flag = 0;
+		}
+	});
+}
